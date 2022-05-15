6c9aedefb601d5363d10a6bc214cdff802ab58cb
max <povasinkirill@gmail.com>
2022-05-15 17:04:18 +0300

    addCards and removeCards finish

--- react.js
+++ react.js
@@ -1,47 +1,63 @@
 class Menu extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
     render() {
         return (
             <div className="menu">
                 <input type="text" placeholder="что ищите?" className="menu__search" />
-                <List changeActiveCard={this.props.changeActiveCard} cards={this.props.cards} removeCard={this.props.removeCard} />
-                <p id="add" onClick={() => this.props.showModal()}>+</p>
+                <List
+                    changeActiveCard={this.props.changeActiveCard}
+                    cards={this.props.cards}
+                    removeCard={this.props.removeCard}
+                    activeNumber = {this.props.activeNumber}
+                />
+                <p id="add" onClick={() => this.props.showModal()}>
+                    +
+                </p>
             </div>
-        )
+        );
     }
 }
 
 class List extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
-    render() {
-        let items = this.props.cards.map((item) => {
-            return(
-            <div className="card" key={item.name} onClick={()=>this.props.changeActiveCard(item)}>
-                <p className="card__name">{item.name}</p>
-                <p className="card__description">{item.description}</p>
-                <div className="card__functions">
-                    <p className="card__functionDelete" onClick={()=>this.props.removeCard(item)}>X</p>
-                    <p className="card__functionRedaction">ред</p>
-                </div>
-            </div>
-            )
 
-        })
-        return (
-            <div>
-                {items}
-            </div>
-        )
+    deleteCard = (index, item) => {
+        this.props.removeCard(index, item);
+    };
+
+    render() {
+        let items = this.props.cards.map((item, index) => {
+            console.log(this.props.activeNumber);
+            return (
+                <div className={`card ${this.props.activeNumber==index?"card_active":null}`} key={item.name} onClick={() => this.props.changeActiveCard(item)}>
+                    <p className="card__name">{item.name}</p>
+                    <p className="card__description">{item.description}</p>
+                    <div className="card__functions">
+                        <p
+                            className="card__functionDelete"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                this.deleteCard(index, item);
+                            }}
+                        >
+                            X
+                        </p>
+                        <p className="card__functionRedaction">ред</p>
+                    </div>
+                </div>
+            );
+        });
+        return <div>{items}</div>;
     }
 }
 
 class Content extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
     }
     render() {
         return (
@@ -49,42 +65,59 @@ class Content extends React.Component {
                 <h1 className="content__name">{this.props.activeCard.name}</h1>
                 <p className="content__description">{this.props.activeCard.description}</p>
             </div>
-        )
+        );
     }
 }
 
-class Modal extends React.Component{
-    constructor(props){
-        super(props)
+class Modal extends React.Component {
+    constructor(props) {
+        super(props);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="notify">
-                <p className="notify__close" onClick={()=>this.props.showModal()}>X</p>
-                <input type="text" placeholder="название" className="notify__name" valuename={this.props.valuename} onChange={this.props.handleChange} />
-                <input type="text" placeholder="описание" className="notify__description" valuedescription={this.props.valuedescription} onChange={this.props.handleChange} />
-                <button className="addBtn" onClick={()=>this.props.saveNewCard}>сохранить</button>
+                <p className="notify__close" onClick={() => this.props.showModal()}>
+                    X
+                </p>
+                <input
+                    type="text"
+                    placeholder="название"
+                    className="notify__name"
+                    value={this.props.valuename}
+                    onChange={this.props.handleChangeName}
+                />
+                <input
+                    type="text"
+                    placeholder="описание"
+                    className="notify__description"
+                    value={this.props.valuedescription}
+                    onChange={this.props.handleChangeDescription}
+                />
+                <button className="addBtn" onClick={this.props.saveNewCard}>
+                    {" "}
+                    сохранить{" "}
+                </button>
             </div>
-        )
-   }
+        );
+    }
 }
 
 class App extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             cards: [
                 {
                     name: "ботинок",
-                    description: "купить ботинок"
+                    description: "купить ботинок",
                 },
                 {
                     name: "бобер",
-                    description: "купить а потом помыть бобра"
+                    description: "купить а потом помыть бобра",
                 },
                 {
                     name: "мага",
-                    description: "продать на рынке магу"
+                    description: "продать на рынке магу",
                 },
             ],
             activeCard: {
@@ -92,68 +125,92 @@ class App extends React.Component {
                 description: "купить ботинок",
             },
             modal: false,
-            valuename: '',
-            valuedescription: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.saveNewCard = this.saveNewCard.bind(this);
+            valuename: "",
+            valuedescription: "",
+        };
     }
-    handleChange(event) {
+    handleChangeName=(event)=> {
         this.setState({
-            valuename: event.target.valuename,
-            valuedescription: event.target.valuedescription
+            valuename: event.target.value
         });
-      }
-      // удаление
-      //удаляет всегда первую карточку как решить
-    removeCard=(item)=>{
-        let removeCards = this.state.cards
-        removeCards.splice(item, 1)
-        this.setState({
-            cards: removeCards
-        }) 
-        console.log(removeCards);
     }
+    handleChangeDescription=(e)=> {
+        this.setState({
+            valuedescription: e.target.value
+        });
+    }
+    removeCard = (index, item) => {
+        let removeCards = this.state.cards;
+        removeCards.splice(index, 1);
+        this.setState({
+            cards: removeCards,
+        });
+        if (this.state.activeCard == item) {
+            this.setState({
+                activeCard: this.state.cards[0]
+            })
+        }
+    };
 
-    changeActiveCard = (card)=>{
+    changeActiveCard = (card) => {
         this.setState({
-            activeCard: card
-        })
-    }
-    
-    showModal=()=> {
+            activeCard: card,
+        });
+    };
+
+    showModal = () => {
         if (!this.state.modal) {
             this.setState({
                 modal: true,
-            })
+            });
         } else if (this.state.modal) {
             this.setState({
-                modal: false
-            })
+                modal: false,
+            });
         }
-    }
-    // добавление 
-    // почему не работает? не готовое решение а обьеснить
-    saveNewCard(event){
+    };
+    saveNewCard=()=> {
         let newCards = this.state.cards;
         newCards.push({
-            name: this.state.valuename ,
-            description: this.state.valuedescription
-        })
-        this.setState({cards: newCards})
-        event.preventDefault();
+            name: this.state.valuename,
+            description: this.state.valuedescription,
+        });
+        this.setState({ cards: newCards });
     }
-    
+
+    getActiveNumber =()=>{
+        for (let i = 0; i < this.state.cards.length; i++) {
+            if (this.state.cards[i].name == this.state.activeCard.name && this.state.cards[i].description == this.state.activeCard.description) {
+                return i
+            } 
+        }
+    }
+
     render() {
         return (
             <div className="app">
-                <Menu changeActiveCard={this.changeActiveCard} showModal={this.showModal} cards={this.state.cards} removeCard={this.removeCard} />
-                <Content activeCard={this.state.activeCard}/>
-                {this.state.modal ? <Modal showModal={this.showModal} saveNewCard={this.saveNewCard} valuename={this.state.valuename} valuedescription={this.state.valuedescription} handleChange={this.handleChange}/> : null}
+                <Menu
+                    changeActiveCard={this.changeActiveCard}
+                    showModal={this.showModal}
+                    cards={this.state.cards}
+                    removeCard={this.removeCard}
+                    activeNumber = {this.getActiveNumber()}
+                />
+                <Content activeCard={this.state.activeCard} />
+                {this.state.modal ? (
+                    <Modal
+                        showModal={this.showModal}
+                        saveNewCard={this.saveNewCard}
+                        valuename={this.state.valuename}
+                        valuedescription={this.state.valuedescription}
+                        handleChangeName={this.handleChangeName}
+                        handleChangeDescription={this.handleChangeDescription}
+                    />
+                ) : null}
             </div>
-        )
+        );
     }
 }
 
-const container = document.getElementById("root")
+const container = document.getElementById("root");
 ReactDOM.render(<App />, container);
